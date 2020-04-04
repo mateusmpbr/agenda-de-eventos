9b0efcc92152b90c91054c7536494c41d4d4beca
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2020-04-04 19:15:34 -0300

    Criando página de listagem

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+def lista_eventos(request):
+    eventos = Evento.objects.all()
+    response = {'eventos':eventos}
+    return render(request, 'agenda.html', response)
