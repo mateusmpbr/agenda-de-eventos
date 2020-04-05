f8451825f2825206e67eca3723115ca13e9d33d5
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2020-04-05 14:39:13 -0300

    Inserção de dados e criação do .gitignore

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('', RedirectView.as_view(url='agenda/')),
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user)
