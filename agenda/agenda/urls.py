2dc81ec5e4fec3343c2fce83ac9de71b22e2af93
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2020-04-05 18:22:41 -0300

    Alteração e exclusão de dados

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 from django.views.generic import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', RedirectView.as_view(url='agenda/')),
@@ -24,7 +25,10 @@ urlpatterns = [
     path('agenda/', views.lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
